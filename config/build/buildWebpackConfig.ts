643c051163c7953cd5bae5aa1ee782380107c96d
21Horza <im.shoo@yandex.ru>
2023-02-03 21:19:58 +0800

    f

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,28 @@
+import { BuildOptions } from "./types/config";
+import webpack from "webpack";
+import { buildLoaders } from "./buildLoaders";
+import { buildResolvers } from "./buildResolvers";
+import { buildPlugins } from "./buildPlugins";
+import path from "path";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode} = options;
+
+    return {
+        mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true,
+        },
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(options)
+          },
+          resolve: buildResolvers(),
+          devtool: options.isDev ? 'inline-source-map' : undefined,
+          devServer: options.isDev ? buildDevServer(options) : undefined,
+    }
+}
