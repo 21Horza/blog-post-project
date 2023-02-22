6033d606a3128137d582f9af37868052a5638ec3
21Horza <im.shoo@yandex.ru>
2023-02-22 23:34:12 +0800

    up

--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -4,14 +4,15 @@ import { within, userEvent } from '@storybook/testing-library';
 import { Page } from './Page';
 
 export default {
-  title: 'Example/Page',
-  component: Page,
-  parameters: {
+    title: 'Example/Page',
+    component: Page,
+    parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
+        layout: 'fullscreen',
+    },
 } as ComponentMeta<typeof Page>;
 
+// eslint-disable-next-line react/jsx-props-no-spreading
 const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
 
 export const LoggedOut = Template.bind({});
@@ -19,8 +20,8 @@ export const LoggedOut = Template.bind({});
 export const LoggedIn = Template.bind({});
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
+LoggedIn.play = async (canvasElement: HTMLElement) => {
+    const canvas = within(canvasElement);
+    const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+    await userEvent.click(loginButton);
 };
