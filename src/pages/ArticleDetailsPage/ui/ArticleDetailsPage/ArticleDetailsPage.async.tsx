7a9fae771eaa36757660e4adaf96b41b353a476e
21Horza <im.shoo@yandex.ru>
2023-05-07 00:30:08 +0800

    add fetching articles, view type selector

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
@@ -3,5 +3,5 @@ import { lazy } from 'react';
 export const ArticleDetailsPageAsync = lazy(() => new Promise((resolve) => {
     // @ts-ignore
     // DO NOT DO THIS IN REAL CASE!
-    setTimeout(() => resolve(import('./ArticleDetailsPage')), 1500);
+    setTimeout(() => resolve(import('./ArticleDetailsPage')), 400);
 }));
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
@@ -3,5 +3,5 @@ import { lazy } from 'react';
 export const ArticlesPageAsync = lazy(() => new Promise((resolve) => {
     // @ts-ignore
     // DO NOT DO THIS IN REAL CASE!
-    setTimeout(() => resolve(import('./ArticlesPage')), 1500);
+    setTimeout(() => resolve(import('./ArticlesPage')), 400);
 }));
