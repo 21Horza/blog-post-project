a4bd8612dc47701e8a4e8d11173254388a0e498e
21Horza <im.shoo@yandex.ru>
2023-06-07 00:20:51 +0800

    production

--- src/pages/AboutPage/UI/AboutPage.tsx
+++ src/pages/AboutPage/UI/AboutPage.tsx
@@ -1,14 +1,13 @@
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Page } from 'widgets/Page/Page';
 
 const AboutPage = () => {
     const { t } = useTranslation('about');
 
     return (
-        <Page>
+        <div>
             {t('About')}
-        </Page>
+        </div>
     );
 };
 
