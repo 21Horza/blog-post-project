012cfbb466fcec815ca123c2b7ec05cf42733348
21Horza <im.shoo@yandex.ru>
2023-06-07 18:31:01 +0800

    new updates

--- src/pages/MainPage/UI/MainPage.tsx
+++ src/pages/MainPage/UI/MainPage.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Page } from 'widgets/Page/Page';
 
 const MainPage = () => {
-    const { t } = useTranslation();
+    const { t } = useTranslation('');
 
     return (
-        <Page>
+        <div>
             {t('Main')}
-        </Page>
+        </div>
     );
 };
 
