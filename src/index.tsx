52cc0c9a413698603093c659c37e12802665f24d
21Horza <im.shoo@yandex.ru>
2023-06-24 18:49:36 +0800

    add vite for development mode

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,6 @@ import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
 import { StoreProvider } from '@/app/providers/StoreProvider';
 import App from './app/App';
-import 'shared/config/i18n/i18n';
-import 'app/styles/index.scss';
 
 const container = document.getElementById('root');
 if (!container) {
